05edb92ceb44bbddfa035b63b89904b8fc973717
Triet Nguyen <140176053+Alrmendo@users.noreply.github.com>
2024-12-17 16:09:57 +0700

    Create Notification Model

---
+++ project/models/NotiModel.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const sub_noti = new mongoose.Schema({
+    content: { type: String, required: true },
+    noti_avatar: { type: String, default: "/images/av1.jpg" },
+    noti_name: { type: String, default: "User" },
+    date: { type: Date, default: Date.now },
+    isRead: { type: Boolean, default: false },
+});
+
+// Main schema for user notifications
+const user_noti = new mongoose.Schema({
+    user_ID: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
+    noti: { type: [sub_noti], default: [] },
+});
+
+const NotificationModel = mongoose.model("Notification", user_noti);
+
+export default NotificationModel;
