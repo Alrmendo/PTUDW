7fb0f45a37291cf11eb7b39130b59513918c3b6c
Triet Nguyen <140176053+Alrmendo@users.noreply.github.com>
2024-12-26 18:28:14 +0700

    add old profile router to fix latest profile

--- project/routes/ProfileRouter.js
+++ project/routes/ProfileRouter.js
@@ -2,16 +2,8 @@ import express from 'express';
 const router = express.Router();
 import ProfileController from '../controllers/ProfileController.js';
 
-import multer from 'multer';
-
-const storage = multer.memoryStorage(); // Store files in memory
-const upload = multer({ storage: storage }).single('avatar');
-// router.get("/", ProfileController.loadUserThreadData, 
-//                 ProfileController.renderProfile);
-
-router.get('/', ProfileController.showProfile);
-
-router.post('/updateProfile', upload, ProfileController.updateProfile);
-router.delete('/unfollow/:followerId', ProfileController.unfollow)
+router.get("/", ProfileController.loadUserThreadData)
+router.get("/", ProfileController.loadFollowsData)
+router.get("/", ProfileController.renderProfile)
 
 export default router;
